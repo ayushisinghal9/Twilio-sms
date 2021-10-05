967ee178064d1bdf6fa65e4ea7d2cc82b7011d1c
ayushisinghal9 <50760849+ayushisinghal9@users.noreply.github.com>
2021-10-05 10:26:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
-
+console.log("Hello");
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
